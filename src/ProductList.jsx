b1a0637835ce0aafb09e40a7a88211168b5184f7
“Gabo-Dev” <jonathangope10@gmail.com>
2024-07-28 10:50:54 -0400

    cart functions fixed

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
-import { useDispatch,useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
-    const totalNumberItems = useSelector(state => state.cart.items);
-    
+    const cart = useSelector(state => state.cart.items);
 
-    
+
+
     const plantsArray = [
         {
             category: "Air Purifying Plants",
@@ -260,7 +260,7 @@ function ProductList() {
             [plant.name]: true, // Set the product name as key and value as true to indicate it's added to cart
         }));
     }
-
+    const totalNumberItems = cart.reduce((total, item) => total + item.quantity, 0);
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -283,7 +283,9 @@ function ProductList() {
                             <circle cx="80" cy="216" r="12"></circle>
                             <circle cx="184" cy="216" r="12"></circle>
                             <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
-                            <text x="120" y="150" >{totalNumberItems.length}</text>
+                            <text x="120" y="150" >
+                                {totalNumberItems}
+                            </text>
                         </svg></h1></a></div>
                 </div>
             </div>
